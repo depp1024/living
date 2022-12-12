7467aeb071f16147c1c59930b686baa8155e4ad6
wakayama.yuki <wakayama.yuki@jp.fujitsu.com>
2022-12-12 19:26:18 +0900

    場所に対するコメントをAPIで取得するよう追加実装しました。

--- js/contents/people.js
+++ js/contents/people.js
@@ -62,6 +62,27 @@ export class People {
         return item.trim().replace(/\s+/g, " ").split(" ");
       });
 
+    const routingCommentString = playerData["routing-comment"];
+    const routingCommentObject = routingCommentString
+      .trim()
+      .split("|")
+      .map(function (item) {
+        const array = item.trim().replace(/\s+/g, " ").split(" ");
+        let keyValue = null;
+        if (array.length > 0) {
+          const key = array[0];
+          array.shift();
+          keyValue = { key, array };
+        }
+        return keyValue;
+      });
+    this.routingCommentMap = new Map();
+    routingCommentObject.forEach((element) => {
+      if (this.routingCommentMap.has(element.key) == false) {
+        this.routingCommentMap.set(element.key, element.array);
+      }
+    });
+
     this.L = L;
     this.map = map;
     this.rectLatLng = rectLatLng;
